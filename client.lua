9eff0ad973c3589b5e98930819446cc3db8e9f29
xinerki <simasadves@gmail.com>
2021-01-06 21:31:55 +0100

    idk man

--- client.lua
+++ client.lua
@@ -57,7 +57,7 @@ safehouses = {
 	},
 	
 	cabdep = {
-		position = vector3(-72.3, -1821.35, 26.9),
+		position = vector3(-72.3, -1821.35, 27.9),
 		destination = vector4(971.1, -2991.1, -39.65, -180.0),
 	},
 	cabdepExit = {
@@ -67,25 +67,25 @@ safehouses = {
 	
 	cabdepVehicle = {
 		vehicle = true,
-		position = vector3(-70.9, -1822.4, 26.45),
+		position = vector3(-70.9, -1822.4, 26.95),
 		destination = vector3(971.65, -2994.35, -40.15),
 		rotation = vector3(0.0, 0.0, -173.0),
 	},	
 	cabdepVehicleExit = {
 		vehicle = true,
-		position = vector3(970.75, -2989.85, -40.15),
+		position = vector3(970.75, -2989.85, -39.65),
 		destination = vector3(-68.1, -1825.55, 26.45),
 		rotation = vector3(0.0, 0.0, -140.85),
 	},
 	
-	niko = {
-		position = vector3(-1733.4, 379.7, 89.7),
-		destination = vector4(-681.35, 591.3, 145.35, -130.0),
-	},
-	nikoExit = {
-		position = vector3(-682.4, 592.45, 145.35),
-		destination = vector4(-1734.35, 381.7, 89.7, 30.85),
-	},
+	-- niko = {
+		-- position = vector3(-1733.4, 379.7, 89.7),
+		-- destination = vector4(-681.35, 591.3, 145.35, -130.0),
+	-- },
+	-- nikoExit = {
+		-- position = vector3(-682.4, 592.45, 145.35),
+		-- destination = vector4(-1734.35, 381.7, 89.7, 30.85),
+	-- },
 	
 	club = {
 		position = vector3(-430.3, -24.3, 46.2),
@@ -118,6 +118,37 @@ safehouses = {
 		rotation = vector3(0.0, 0.0, 86.5),
 	},
 	
+	clubBasement = {
+		position = vector3(-1618.5, -3007.45, -75.25),
+		destination = vector4(-1507.75, -3014.45, -79.25, 0.0),
+	},
+	clubBasementExit = {
+		position = vector3(-1507.65, -3017.15, -79.25),
+		destination = vector4(-1617.85, -3008.85, -75.25, -146.75),
+	},
+	
+	clubBasementGarage = {
+		position = vector3(-1640.05, -3015.0, -78.15),
+		destination = vector4(-1520.1, -2978.85, -79.55, -90.0),
+	},	
+	clubBasementGarageExit = {
+		position = vector3(-1521.95, -2978.85, -80.4),
+		destination = vector4(-1639.85, -3013.3, -78.15, 0.0),
+	},
+	
+	clubBasementGarageVehicle = {
+		vehicle = true,
+		position = vector3(-1639.9, -3013.05, -78.0),
+		destination = vector3(-1514.65, -2978.95, -81.35),
+		rotation = vector3(-5.30, -0.0, -90.05),
+	},	
+	clubBasementGarageVehicleExit = {
+		vehicle = true,
+		position = vector3(-1519.85, -2978.95, -80.4),
+		destination = vector3(-1639.9, -3008.55, -78.85),
+		rotation = vector3(0.0, 0.0, 0.0),
+	},
+	
 	muriel = {
 		position = vector3(307.55, -1005.6, 29.3),
 		destination = vector4(346.6, -1004.7, -99.2, 0.6),
@@ -125,6 +156,15 @@ safehouses = {
 	murielExit = {
 		position = vector3(346.5, -1012.8, -99.2),
 		destination = vector4(307.55, -1003.25, 29.3, 2.25),
+	},
+	
+	switch = {
+		position = vector3(-614.65, 46.25, 43.55),
+		destination = vector4(-605.6, 58.95, 98.2, 90.0)
+	},
+	switchExit = {
+		position = vector3(-603.0, 58.9, 98.2),
+		destination = vector4(-614.65, 43.05, 43.55, -180.0),
 	},
 	
 	casino = {
@@ -154,17 +194,66 @@ safehouses = {
 	casinoGarageVehExit = {
 		vehicle = true,
 		position = vector3(1336.75, 190.6, -48.35),
-		destination = vector3(931, -6.0, 78.3),
+		destination = vector3(931.0, -6.0, 78.3),
 		rotation = vector3(0.0, 0.0, 147.8),
 	},
 	
-	switch = {
-		position = vector3(-614.65, 46.25, 43.55),
-		destination = vector4(-605.6, 58.95, 98.2, 90.0)
+	arcade = {
+		position = vector3(758.75, -816.15, 26.25),
+		destination = vector4(2734.95, -377.2, -48.05, 90.0),
 	},
-	switchExit = {
-		position = vector3(-603.0, 58.9, 98.2),
-		destination = vector4(-614.65, 43.05, 43.55, -180.0),
+	arcadeExit = {
+		position = vector3(2737.9, -374.35, -48.0),
+		destination = vector4(760.65, -816.1, 26.3, -90.0),
+	},
+	
+	arcadeGarage = {
+		position = vector3(726.8, -822.85, 24.85),
+		destination = vector4(2679.55, -361.45, -55.2, -90.0),
+	},
+	arcadeGarageExit = {
+		position = vector3(2677.55, -361.45, -55.2),
+		destination = vector4(724.75, -822.55, 24.75, 90.0),
+	},
+	
+	arcadeGarageVeh = {
+		vehicle = true,
+		position = vector3(725.5, -822.5, 24.75),
+		destination = vector3(2685.05, -361.35, -55.85),
+		rotation = vector3(-0.0, 0.0, -90.0),
+	},
+	arcadeGarageVehExit = {
+		vehicle = true,
+		position = vector3(2679.45, -361.35, -55.25),
+		destination = vector3(722.7, -829.2, 24.0),
+		rotation = vector3(0.0, 0.0, -180.0),
+	},
+	
+	penthouse = {
+		position = vector3(1085.45, 214.35, -49.25),
+		destination = vector4(979.45, 57.25, 116.15, 60.65),
+	},
+	penthouseExit = {
+		position = vector3(980.55, 56.6, 116.15),
+		destination = vector4(1086.1, 215.15, -49.25, -41.1),
+	},
+	
+	terracePenthouse = {
+		position = vector3(967.8, 63.7, 112.55),
+		destination = vector4(970.8, 62.85, 112.55, -120.0),
+	},
+	terracePenthouseExit = {
+		position = vector3(969.5, 63.15, 112.55),
+		destination = vector4(966.7, 63.95, 112.55, 81.8),
+	},
+	
+	theo = {
+		position = vector3(-852.96, 694.074, 149.04),
+		destination = vector4(-859.85, 689.82, 152.86, 180.0)
+	},
+	theoExit = {
+		position = vector3(-859.95, 691.21, 152.86),
+		destination = vector4(-853.03, 695.39, 148.78, 0.0),
 	},
 	
 }
@@ -180,44 +269,50 @@ Citizen.CreateThread(function()
 	DoScreenFadeIn(1000)
 	while true do
 		for i,v in pairs(safehouses) do
-			if v.vehicle ~= true then
-				if not IsPedInAnyVehicle(PlayerPedId(), false) then
-					DrawMarker(1, v.position.x, v.position.y, v.position.z-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.3, 255, 255, 100, 150, false, false, false, true)
-					-- DrawMarker(0, v.position.x, v.position.y, v.position.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.8, 0.8, 1.0, 255, 255, 100, 150, true, false, false, true)
-					-- DrawMarker(2, v.position.x, v.position.y, v.position.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.8, 0.8, -1.0, 255, 255, 100, 50, false, true, false, false)
-					-- DrawMarker(1, v.position.x, v.position.y, v.position.z-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 255, 255, 100, 100, false, false, false, true)
-					local pos = GetEntityCoords(PlayerPedId())
-					if GetDistanceBetweenCoords(pos, v.position, true) < 1.0 then
-						DoScreenFadeOut(1000)
-						SetPlayerControl(PlayerId(), false, 0)
-						Citizen.Wait(1000)
-						SetEntityCoords(PlayerPedId(), v.destination.x, v.destination.y, v.destination.z, 0.0, 0.0, 0.0, true)
-						SetEntityRotation(PlayerPedId(), 0.0, 0.0, v.destination.w, 0, true)
-						FreezeEntityPosition(PlayerPedId(), true)
-						repeat Wait(0) until HasCollisionLoadedAroundEntity(PlayerPedId())
-						Citizen.Wait(300)
-						SetPlayerControl(PlayerId(), true, 0)
-						FreezeEntityPosition(PlayerPedId(), false)
-						DoScreenFadeIn(1000)
+			if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), v.position, true) < 10.0 then
+				if v.vehicle ~= true then
+					if not IsPedInAnyVehicle(PlayerPedId(), false) then
+						DrawMarker(1, v.position.x, v.position.y, v.position.z-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.3, 255, 255, 100, 150, false, false, false, true)
+						-- DrawMarker(0, v.position.x, v.position.y, v.position.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.8, 0.8, 1.0, 255, 255, 100, 150, true, false, false, true)
+						-- DrawMarker(2, v.position.x, v.position.y, v.position.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.8, 0.8, -1.0, 255, 255, 100, 50, false, true, false, false)
+						-- DrawMarker(1, v.position.x, v.position.y, v.position.z-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 255, 255, 100, 100, false, false, false, true)
+						local pos = GetEntityCoords(PlayerPedId())
+						if GetDistanceBetweenCoords(pos, v.position, true) < 1.0 then
+							DoScreenFadeOut(1000)
+							SetPlayerControl(PlayerId(), false, 0)
+							Citizen.Wait(1000)
+							SetEntityCoords(PlayerPedId(), v.destination.x, v.destination.y, v.destination.z-1.0, 0.0, 0.0, 0.0, true)
+							SetEntityRotation(PlayerPedId(), 0.0, 0.0, v.destination.w, 0, true)
+							FreezeEntityPosition(PlayerPedId(), true)
+							repeat Wait(0) until HasCollisionLoadedAroundEntity(PlayerPedId())
+							SetGameplayCamRelativeHeading(0.0)
+							SetPedDesiredHeading(PlayerPedId(), v.destination.w)
+							Citizen.Wait(300)
+							SetPlayerControl(PlayerId(), true, 0)
+							FreezeEntityPosition(PlayerPedId(), false)
+							DoScreenFadeIn(1000)
+						end
 					end
-				end
-			else
-				if IsPedInAnyVehicle(PlayerPedId(), false) then
-					DrawMarker(1, v.position.x, v.position.y, v.position.z-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 3.0, 3.0, 0.3, 255, 255, 100, 150, false, false, false, true)
-					-- DrawMarker(1, v.position.x, v.position.y, v.position.z-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 0.5, 255, 255, 100, 100, false, false, false, true)
-					local pos = GetEntityCoords(GetVehiclePedIsIn(PlayerPedId(), false))
-					if GetDistanceBetweenCoords(pos, v.position, true) < 3.0 then
-						DoScreenFadeOut(1000)
-						SetPlayerControl(PlayerId(), false, 0)
-						Citizen.Wait(1000)
-						SetEntityCoords(GetVehiclePedIsIn(PlayerPedId(), false), v.destination.x, v.destination.y, v.destination.z-0.5, 0.0, 0.0, 0.0, true)
-						SetEntityRotation(GetVehiclePedIsIn(PlayerPedId(), false), v.rotation.x, v.rotation.y, v.rotation.z, 0, true)
-						FreezeEntityPosition(GetVehiclePedIsIn(PlayerPedId(), false), true)
-						repeat Wait(0) until HasCollisionLoadedAroundEntity(GetVehiclePedIsIn(PlayerPedId(), false))
-						Citizen.Wait(300)
-						SetPlayerControl(PlayerId(), true, 0)
-						FreezeEntityPosition(GetVehiclePedIsIn(PlayerPedId(), false), false)
-						DoScreenFadeIn(1000)
+				else
+					if IsPedInAnyVehicle(PlayerPedId(), false) then
+						DrawMarker(1, v.position.x, v.position.y, v.position.z-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 3.0, 3.0, 0.3, 255, 255, 100, 150, false, false, false, true)
+						-- DrawMarker(1, v.position.x, v.position.y, v.position.z-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 0.5, 255, 255, 100, 100, false, false, false, true)
+						local pos = GetEntityCoords(GetVehiclePedIsIn(PlayerPedId(), false))
+						if GetDistanceBetweenCoords(pos, v.position, true) < 3.0 then
+							DoScreenFadeOut(1000)
+							SetPlayerControl(PlayerId(), false, 0)
+							Citizen.Wait(1000)
+							SetEntityCoords(GetVehiclePedIsIn(PlayerPedId(), false), v.destination.x, v.destination.y, v.destination.z-0.5, 0.0, 0.0, 0.0, true)
+							SetEntityRotation(GetVehiclePedIsIn(PlayerPedId(), false), v.rotation.x, v.rotation.y, v.rotation.z, 0, true)
+							FreezeEntityPosition(GetVehiclePedIsIn(PlayerPedId(), false), true)
+							repeat Wait(0) until HasCollisionLoadedAroundEntity(GetVehiclePedIsIn(PlayerPedId(), false))
+							SetGameplayCamRelativeHeading(0.0)
+							Citizen.Wait(300)
+							SetPlayerControl(PlayerId(), true, 0)
+							FreezeEntityPosition(GetVehiclePedIsIn(PlayerPedId(), false), false)
+							SetVehicleOnGroundProperly(GetVehiclePedIsIn(PlayerPedId(), false))
+							DoScreenFadeIn(1000)
+						end
 					end
 				end
 			end
